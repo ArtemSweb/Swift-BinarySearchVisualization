95e38733d1c037d10d6f702ebd02080a14a67ae0
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-05-27 08:32:20 +0300

    Initial Commit

---
+++ BinarySearchVisualization/BinarySearchVisualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinarySearchVisualizationApp.swift
+//  BinarySearchVisualization
+//
+//  Created by Артем Солодовников on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BinarySearchVisualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BinarySearchVisualization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BinarySearchVisualization
+//
+//  Created by Артем Солодовников on 27.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
